4bc167b463407a43420e58eef65b19d09d0e8df9
Paul Mwaura <paulmwaura555@gmail.com>
2025-04-20 21:35:36 -0400

    Added: Minor UI changes to homepage

--- sections/homePage/Hero.jsx
+++ sections/homePage/Hero.jsx
@@ -4,9 +4,9 @@ export const Hero = () => {
     return (
         <section>
             <div className="container h-[100vh] flex flex-col justify-center items-center -mt-10">
-                <strong className="text-lg md:text-2xl tracking-tight text-purple-400">I Make It Easier</strong>
+                <p className="tag outline-orange-500 opacity-90">A Service Marketplace</p>
+                <strong className="mt-2 text-lg md:text-2xl tracking-tight text-purple-400">Make It Easier</strong>
                 <strong className="text-xl md:text-3xl tracking-tight">For Your Customers To Find You</strong>
-                <p className="mt-2 tag outline-orange-500 opacity-90">A service marketplace that helps you manage your gigs</p>
                 <div className="mt-5 flex flex-row items-center gap-5">
                     <div className="flex flex-col text-center">
                         <Link href={"applyLister"} className="btn outline-1">Create A Lister's Account</Link>
