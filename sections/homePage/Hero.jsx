0a2f7b689a878be203dbe66420a182b1fa1af4f2
Paul Mwaura <paulmwaura555@gmail.com>
2025-04-23 09:57:24 -0400

    Added: Minor UI text changes to homepage

--- sections/homePage/Hero.jsx
+++ sections/homePage/Hero.jsx
@@ -4,8 +4,8 @@ export const Hero = () => {
     return (
         <section>
             <div className="container h-[100vh] flex flex-col justify-center items-center -mt-16">
-                <p className="tag outline-orange-500 opacity-90">A Service Marketplace</p>
-                <strong className="mt-2 text-lg md:text-2xl tracking-tight text-purple-400">Make It Easier</strong>
+                <p className="tag outline-orange-500 opacity-90">The Service Marketplace</p>
+                <strong className="mt-2 text-lg md:text-2xl tracking-tight text-purple-400">Where We Make It Easier</strong>
                 <strong className="text-xl md:text-3xl tracking-tight">For Your Customers To Find You</strong>
                 <div className="mt-5 flex flex-row items-center gap-5">
                     <div className="flex flex-col text-center">
