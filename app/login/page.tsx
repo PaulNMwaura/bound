f1e43011d7bfbe751afc6c6faaeb074e91c82fbe
Paul Mwaura <paulmwaura555@gmail.com>
2025-11-13 10:25:55 -0500

    Type error fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/auth.config";
 
-export default async function Login(req:any) {
+export default async function Login() {
     // if the user is already signed in, they shouldn't be accessing the login page
     const session = await getServerSession(authOptions);
     if (session) redirect("/");
