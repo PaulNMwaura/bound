27942c776aa8a0b8c5b33f5f947b99570e18ea25
Paul Mwaura <paulmwaura555@gmail.com>
2025-11-25 22:09:07 -0500

    Fixed: Middleware error. Converted to proxy

--- middleware.js
+++
@@ -1,5 +0,0 @@
-export { default }from "next-auth/middleware";
-
-export const config = {
-    matcher: ["/applyLister", "/dashboard/:path*", "/messages/:path*", "/settings", "/settings/:path*", "/payment"]
-};
